ccc8255c6f71c6c38d6f94a57ec896da8eb448ef
MosesVishwa <moses.vishwa@griffithuni.edu.au>
2023-09-16 16:08:05 +1000

    q1 done

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,23 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+connection = sqlite3.connect('climate.db')
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM ClimateData")
+
+results = cursor.fetchall()
+
 years = []
 co2 = []
 temp = []
+
+for r in results:
+    years.append(r[0])
+    co2.append(r[1])
+    temp.append(r[2])
+
+cursor.close()
+connection.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -13,6 +28,8 @@ plt.xlabel("Year (decade)")
 plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_1.png")
+
+
